d6638774fe6e03fa0e712d8e0777dba5948d1a63
zenithfalling <zenithfalling@outlook.com>
2026-08-19 16:39:51 +0800

    Initial commit
    PWM output and change PWM duty while running

---
+++ MDK-ARM/RTE/_stm32-fan-controller/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32-fan-controller' 
+ * Target:  'stm32-fan-controller' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
